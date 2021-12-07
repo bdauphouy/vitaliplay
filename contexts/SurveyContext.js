6225a922ccaf483b05ec1820d71b6ca15bf5db81
Baptiste <bdauphouy@gmail.com>
2021-12-07 20:31:39 +0100

    feat: activeStep stored in localStorage

--- contexts/SurveyContext.js
+++ contexts/SurveyContext.js
@@ -1,12 +1,92 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const SurveyContext = createContext()
 
 export const SurveyContextProvider = ({ children }) => {
   const [store, setStore] = useState({})
+  const [prefix] = useState('/survey')
+
+  const [surveySteps] = useState([
+    { id: 1, step: 'Inscription', path: '/signup' },
+    { id: 2, step: 'Mensuration', path: '/measurements' },
+    { id: 3, step: 'Fumeur', path: '/smoker' },
+    { id: 4, step: 'Douleurs chroniques', path: '/pain' },
+    { id: 5, step: 'Affection longue durée', path: '/affection' },
+    { id: 6, step: 'Prothèse articulaire', path: '/prosthesis' },
+    { id: 7, step: 'Succès', path: '/success', hidden: true },
+  ])
+
+  const [activeStep, setActiveStep] = useState('1')
+
+  useEffect(() => {
+    if (!window.localStorage.getItem('vitaliplay.survey.activeStep')) {
+      window.localStorage.setItem('vitaliplay.survey.activeStep', '1')
+    }
+    setActiveStep(window.localStorage.getItem('vitaliplay.survey.activeStep'))
+  }, [])
+
+  const getPathById = id => {
+    let path = null
+
+    surveySteps.map(surveyStep => {
+      if (surveyStep.id === id) {
+        path = surveyStep.path
+      }
+    })
+
+    return path
+  }
+
+  const getPathByStep = step => {
+    let path = null
+
+    surveySteps.map(surveyStep => {
+      if (surveyStep.step === step) {
+        path = surveyStep.path
+      }
+    })
+
+    return path
+  }
+
+  const getIdByStep = step => {
+    let id = null
+
+    surveySteps.map(surveyStep => {
+      if (surveyStep.step === step) {
+        id = surveyStep.id
+      }
+    })
+
+    return id
+  }
+
+  const getIdByPath = path => {
+    let id = null
+
+    surveySteps.map(surveyStep => {
+      if (surveyStep.path === path) {
+        id = surveyStep.id
+      }
+    })
+
+    return id
+  }
 
   return (
-    <SurveyContext.Provider value={{ store, setStore }}>
+    <SurveyContext.Provider
+      value={{
+        store,
+        setStore,
+        prefix,
+        activeStep,
+        setActiveStep,
+        getPathById,
+        getIdByStep,
+        getIdByPath,
+        getPathByStep,
+        surveySteps,
+      }}>
       {children}
     </SurveyContext.Provider>
   )
--- pages/survey/index.js
+++ pages/survey/index.js
@@ -1,11 +1,19 @@
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
+import { SurveyContext } from '@/contexts/SurveyContext'
 
 const Survey = () => {
   const router = useRouter()
 
+  const { prefix, activeStep, getPathById } = useContext(SurveyContext)
+
   useEffect(() => {
-    router.push('/survey/signup')
+    console.log(activeStep)
+    router.push(
+      `${prefix}${getPathById(
+        parseInt(window.localStorage.getItem('vitaliplay.survey.activeStep')),
+      )}`,
+    )
   }, [])
 
   return <></>
